9d8fdc3cff85a1ce35577cd1efe039b45fb221f3
Aurélien Grimaud <a.grimaud@involt.io>
2022-04-20 23:12:02 +0200

    Inject the rich external link in the Id/Network

--- src/views/Id.vue
+++ src/views/Id.vue
@@ -3,7 +3,9 @@
   import Footer from '@/components/patterns/Footer.vue'
   import Button from '@/components/ui/Button.vue'
   import OneColumn from '@/components/layouts/OneColumn.vue'
-  import { RotateCcw } from 'lucide-vue-next'
+  import TwoColumns from '@/components/layouts/TwoColumns.vue'
+  import RichExternalLink from '@/components/ui/RichExternalLink.vue'
+  import { RotateCcw, Twitter, Dribbble, BookOpen, Codepen, Github } from 'lucide-vue-next'
 </script>
 
 <template>
@@ -36,6 +38,82 @@
             <p v-html="$t('id.biography.side')"></p>
           </template>
         </OneColumn>
+      </section>
+      <section>
+        <TwoColumns
+          :title="$t('id.network.title')"
+        >
+          <template #left>
+            <RichExternalLink
+              :title="$t('id.network.twitter.title')"
+              :description="$t('id.network.twitter.description')"
+              color="var(--color-soft-wind)"
+              :href="$t('id.network.twitter.href')"
+              :alt="$t('id.network.twitter.alt')"
+            >
+              <template #icon>
+                <Twitter
+                  :size="48"
+                />
+              </template>
+            </RichExternalLink>
+            <RichExternalLink
+              :title="$t('id.network.codepen.title')"
+              :description="$t('id.network.codepen.description')"
+              color="var(--color-deep-black)"
+              :href="$t('id.network.codepen.href')"
+              :alt="$t('id.network.codepen.alt')"
+            >
+              <template #icon>
+                <Codepen
+                  :size="48"
+                  stroke="var(--color-titanium-white)"
+                />
+              </template>
+            </RichExternalLink>
+            <RichExternalLink
+              :title="$t('id.network.github.title')"
+              :description="$t('id.network.github.description')"
+              color="var(--color-creamy-sun)"
+              :href="$t('id.network.github.href')"
+              :alt="$t('id.network.github.alt')"
+            >
+              <template #icon>
+                <Github
+                  :size="48"
+                />
+              </template>
+            </RichExternalLink>
+          </template>
+          <template #right>
+            <RichExternalLink
+              :title="$t('id.network.dribbble.title')"
+              :description="$t('id.network.dribbble.description')"
+              color="var(--color-candy-floss)"
+              :href="$t('id.network.dribbble.href')"
+              :alt="$t('id.network.dribbble.alt')"
+            >
+              <template #icon>
+                <Dribbble
+                  :size="48"
+                />
+              </template>
+            </RichExternalLink>
+            <RichExternalLink
+              :title="$t('id.network.medium.title')"
+              :description="$t('id.network.medium.description')"
+              color="var(--color-titanium-white)"
+              :href="$t('id.network.medium.href')"
+              :alt="$t('id.network.medium.alt')"
+            >
+              <template #icon>
+                <BookOpen
+                  :size="48"
+                />
+              </template>
+            </RichExternalLink>
+          </template>
+        </TwoColumns>
       </section>
     </article>
     <Footer
