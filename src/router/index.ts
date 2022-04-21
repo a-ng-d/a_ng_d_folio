edd68d91bd5782b7ffec0abb95479a8a2cac75af
Aurélien Grimaud <a.grimaud@involt.io>
2022-04-22 00:50:14 +0200

    Open external link with RouterLink

--- src/router/index.ts
+++ src/router/index.ts
@@ -51,7 +51,21 @@ const router = createRouter({
       path: '/_attribution',
       name: 'attribution',
       component: Home
-    }
+    },
+    {
+      path: '/_resume',
+      name: 'resume',
+      beforeEnter() {
+        window.open('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/f2cf96ad-5468-4704-b18a-c8761a0d5588/Aurelien_Grimaud・Resume・2022.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220421%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220421T224043Z&X-Amz-Expires=86400&X-Amz-Signature=d90b99058da0d8897ba37992a1abb83d6d0713316803296c7ce0edafdd157a5f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D"Aurelien%2520Grimaud%25E3%2583%25BBResume%25E3%2583%25BB2022.pdf"&x-id=GetObject', '_blank')
+      }
+    },
+    {
+      path: '/_linkedin',
+      name: 'linkedin',
+      beforeEnter() {
+        window.open('https://www.linkedin.com/in/augrimaud/', '_blank')
+      }
+    },
   ]
 })
 
