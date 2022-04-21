ff32c83ac6e2db7985f7ea749bf2a5fb9fae9cc4
Aurélien Grimaud <a.grimaud@involt.io>
2022-04-21 23:43:39 +0200

    Inject contexts instead of pure html

--- src/views/Id.vue
+++ src/views/Id.vue
@@ -1,12 +1,35 @@
-<script setup lang="ts">
+<script lang="ts">
   import MainMenu from '@/components/patterns/MainMenu.vue'
   import Footer from '@/components/patterns/Footer.vue'
   import Button from '@/components/ui/Button.vue'
-  import OneColumn from '@/components/layouts/OneColumn.vue'
-  import TwoColumns from '@/components/layouts/TwoColumns.vue'
-  import RichExternalLink from '@/components/ui/RichExternalLink.vue'
-  import SimpleExternalLink from '@/components/ui/SimpleExternalLink.vue'
-  import { RotateCcw, Twitter, Dribbble, BookOpen, Codepen, Github, Linkedin, Download } from 'lucide-vue-next'
+  import About from '@/contexts/About.vue'
+  import Networks from '@/contexts/Networks.vue'
+  import Career from '@/contexts/Career.vue'
+  import Stories from '@/contexts/Stories.vue'
+  import Talks from '@/contexts/Talks.vue'
+  import Workshops from '@/contexts/Workshops.vue'
+  import { RotateCcw } from 'lucide-vue-next'
+
+  export default {
+    name: "Id",
+    components: {
+      MainMenu,
+      Footer,
+      Button,
+      About,
+      Networks,
+      Career,
+      Stories,
+      Talks,
+      Workshops,
+      RotateCcw
+    },
+    data() {
+      return {
+        theme: "default"
+      }
+    }
+  }
 </script>
 
 <template>
@@ -28,235 +51,12 @@
       </template>
     </MainMenu>
     <article class="article">
-      <section>
-        <OneColumn
-          :title="$t('id.biography.title')"
-        >
-          <template #plain>
-            <div>
-              <p v-html="$t('id.biography.experience')"></p>
-              <p>{{ $t("id.biography.mission") }}</p>
-              <p v-html="$t('id.biography.activities')"></p>
-              <p>{{ $t("id.biography.side") }}</p>
-            </div>
-          </template>
-        </OneColumn>
-      </section>
-      <section>
-        <TwoColumns
-          :title="$t('id.network.title')"
-        >
-          <template #left>
-            <RichExternalLink
-              :title="$t('id.network.twitter.title')"
-              :description="$t('id.network.twitter.description')"
-              color="var(--color-soft-wind)"
-              :href="$t('id.network.twitter.href')"
-              :alt="$t('id.network.twitter.alt')"
-            >
-              <template #icon>
-                <Twitter
-                  :size="48"
-                />
-              </template>
-            </RichExternalLink>
-            <RichExternalLink
-              :title="$t('id.network.codepen.title')"
-              :description="$t('id.network.codepen.description')"
-              color="var(--color-deep-black)"
-              :href="$t('id.network.codepen.href')"
-              :alt="$t('id.network.codepen.alt')"
-            >
-              <template #icon>
-                <Codepen
-                  :size="48"
-                  stroke="var(--color-titanium-white)"
-                />
-              </template>
-            </RichExternalLink>
-            <RichExternalLink
-              :title="$t('id.network.github.title')"
-              :description="$t('id.network.github.description')"
-              color="var(--color-creamy-sun)"
-              :href="$t('id.network.github.href')"
-              :alt="$t('id.network.github.alt')"
-            >
-              <template #icon>
-                <Github
-                  :size="48"
-                />
-              </template>
-            </RichExternalLink>
-          </template>
-          <template #right>
-            <RichExternalLink
-              :title="$t('id.network.dribbble.title')"
-              :description="$t('id.network.dribbble.description')"
-              color="var(--color-candy-floss)"
-              :href="$t('id.network.dribbble.href')"
-              :alt="$t('id.network.dribbble.alt')"
-            >
-              <template #icon>
-                <Dribbble
-                  :size="48"
-                />
-              </template>
-            </RichExternalLink>
-            <RichExternalLink
-              :title="$t('id.network.medium.title')"
-              :description="$t('id.network.medium.description')"
-              color="var(--color-titanium-white)"
-              :href="$t('id.network.medium.href')"
-              :alt="$t('id.network.medium.alt')"
-            >
-              <template #icon>
-                <BookOpen
-                  :size="48"
-                />
-              </template>
-            </RichExternalLink>
-          </template>
-        </TwoColumns>
-      </section>
-      <section>
-        <TwoColumns
-          :title="$t('id.career.title', { year: new Date().getFullYear() - 2015 })"
-        >
-          <template #left>
-            <Button type="secondary" path="/" layout="indicator">
-              <template #icon>
-                <Download
-                  :size="24"
-                />
-              </template>
-              <template #label>
-                {{ $t("id.career.resume") }}
-              </template>
-            </Button>
-          </template>
-          <template #right>
-            <Button type="secondary" path="/" layout="indicator">
-              <template #icon>
-                <Linkedin
-                  :size="24"
-                />
-              </template>
-              <template #label>
-                {{ $t("id.career.linkedIn") }}
-              </template>
-            </Button>
-          </template>
-        </TwoColumns>
-        <OneColumn>
-          <template #plain>
-            <div>
-              <h5>
-                <span>{{ $t("id.career.ecv.start") }}</span>
-                <span>{{ $t("global.separator") }}</span>
-                <span>{{ $t("id.career.ecv.end") }}</span>
-              </h5>
-              <h4>{{ $t("id.career.ecv.title") }}</h4>
-              <p>{{ $t("id.career.ecv.label") }}</p>
-            </div>
-            <div>
-              <h5>
-                <span>{{ $t("id.career.iobeya.start") }}</span>
-                <span>{{ $t("global.separator") }}</span>
-                <span>{{ $t("id.career.iobeya.end") }}</span>
-              </h5>
-              <h4>{{ $t("id.career.iobeya.title") }}</h4>
-              <div>
-                <h6>
-                  <span>{{ $t("id.career.iobeya.uxDesign.start") }}</span>
-                  <span>{{ $t("global.separator") }}</span>
-                  <span>{{ $t("id.career.iobeya.uxDesign.end") }}</span>
-                </h6>
-                <p>{{ $t("id.career.iobeya.uxDesign.title") }}</p>
-              </div>
-              <div>
-                <h6>
-                  <span>{{ $t("id.career.iobeya.productDesign.start") }}</span>
-                  <span>{{ $t("global.separator") }}</span>
-                  <span>{{ $t("id.career.iobeya.productDesign.end") }}</span>
-                </h6>
-                <p>{{ $t("id.career.iobeya.productDesign.title") }}</p>
-              </div>
-              <div>
-                <h6>
-                  <span>{{ $t("id.career.iobeya.creativeTechnologist.start") }}</span>
-                  <span>{{ $t("global.separator") }}</span>
-                  <span>{{ $t("id.career.iobeya.creativeTechnologist.end") }}</span>
-                </h6>
-                <p>{{ $t("id.career.iobeya.creativeTechnologist.title") }}</p>
-              </div>
-            </div>
-          </template>
-        </OneColumn>
-      </section>
-      <section>
-        <OneColumn
-          :title="$t('id.stories.title')"
-        >
-          <template #plain>
-            <SimpleExternalLink
-              :label="$t('id.stories.link-1.label')"
-              :href="$t('id.stories.link-1.href')"
-              :alt="$t('id.stories.link-1.alt')"
-            />
-            <SimpleExternalLink
-              :label="$t('id.stories.link-2.label')"
-              :href="$t('id.stories.link-2.href')"
-              :alt="$t('id.stories.link-2.alt')"
-            />
-            <SimpleExternalLink
-              :label="$t('id.stories.link-3.label')"
-              :href="$t('id.stories.link-3.href')"
-              :alt="$t('id.stories.link-3.alt')"
-            />
-            <SimpleExternalLink
-              :label="$t('id.stories.link-4.label')"
-              :href="$t('id.stories.link-4.href')"
-              :alt="$t('id.stories.link-4.alt')"
-            />
-          </template>
-        </OneColumn>
-      </section>
-      <section>
-        <OneColumn
-          :title="$t('id.talks.title')"
-        >
-          <template #plain>
-            <SimpleExternalLink
-              :label="$t('id.talks.link-1.label')"
-              :href="$t('id.talks.link-1.href')"
-              :alt="$t('id.talks.link-1.alt')"
-            />
-            <SimpleExternalLink
-              :label="$t('id.talks.link-2.label')"
-              :href="$t('id.talks.link-2.href')"
-              :alt="$t('id.talks.link-2.alt')"
-            />
-          </template>
-        </OneColumn>
-      </section>
-      <section>
-        <OneColumn
-          :title="$t('id.workshops.title')"
-        >
-          <template #plain>
-            <SimpleExternalLink
-              :label="$t('id.workshops.link-1.label')"
-              :href="$t('id.workshops.link-1.href')"
-              :alt="$t('id.workshops.link-1.alt')"
-            />
-            <SimpleExternalLink
-              :label="$t('id.workshops.link-2.label')"
-              :href="$t('id.workshops.link-2.href')"
-              :alt="$t('id.workshops.link-2.alt')"
-            />
-          </template>
-        </OneColumn>
-      </section>
+      <About :theme="theme" />
+      <Networks :theme="theme" />
+      <Career :theme="theme" />
+      <Stories :theme="theme" />
+      <Talks :theme="theme" />
+      <Workshops :theme="theme" />
     </article>
     <Footer
       alignment="center"
