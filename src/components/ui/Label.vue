a83fd49f9e5457f34f3e26c5d4d2040aa26af4d1
Aurélien Grimaud <a.grimaud@involt.io>
2022-04-21 17:40:26 +0200

    Create an isololated label

---
+++ src/components/ui/Label.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+export default {
+    name: 'Label',
+    props: {
+      label: {
+        type: String,
+        required: true
+      },
+      underlined: {
+        type: Boolean,
+        default: false
+      },
+      theme: {
+        type: String,
+        default: "default"
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="label" :data-theme="theme" :data-underlined="underlined ? true : false">
+    <span>{{ label }}</span>
+  </div>
+</template>
+
+<style lang="sass">
+  @use '@/assets/text-styles.sass' as text
+
+  // Structure
+  .label
+    @include text.current
+
+    span
+      z-index: 1
+
+    &[data-underlined="true"]
+      &:after
+        content: ''
+        width: 100%
+        height: var(--border-size)
+        position: absolute
+        left: 0
+        bottom: calc(var(--border-size) * -1)
+        border-radius: calc(var(--border-size) / 2)
+        transition: var(--simple-transition)
+
+  // Aspect
+  [data-theme="dark"]
+    --text-color: var(--color-cream)
+    --border-color: var(--color-cream)
+
+  .label
+    color: var(--text-color)
+
+    &[data-underlined="true"]
+      &:after
+        background: var(--border-color)
+
+
+
+</style>
--- src/components/ui/SimpleExternalLink.vue
+++ src/components/ui/SimpleExternalLink.vue
@@ -1,41 +1,72 @@
 <script lang="ts">
   import { ExternalLink } from 'lucide-vue-next'
+  import Label from '@/components/ui/Label.vue'
 
   export default {
     name: 'SimpleExternalLink',
     components: {
-      ExternalLink
+      ExternalLink,
+      Label
     },
     props: {
       label: String,
       href: String,
       alt: String,
-      dark: {
-        type: Boolean,
-        default: false
+      theme: {
+        type: String,
+        default: "default"
       }
     }
   }
 </script>
 
 <template>
-  <a class="external-link" :href="href" target="_blank" :alt="alt">
+  <a class="external-link" :href="href" target="_blank" :alt="alt" :data-theme="theme">
     <div class="external-link__icon">
       <ExternalLink
         :size="24"
       />
     </div>
     <div class="external-link__label">
-      <span>{{ label }}</span>
+      <Label
+        :label="label"
+        underlined
+        :theme="theme"
+      />
     </div>
   </a>
 </template>
 
 <style scoped lang="sass">
+  @use '@/assets/text-styles.sass' as text
+
   // Structure
   .external-link
     display: flex
     flex-flow: row nowrap
-    gap: 0 var(--layout-paragraph-gap)
+    gap: 0 var(--rich-external-link-gap)
+
+    &__icon
+      padding: var(--spacing-s-000) 0 0
+
+  // Aspect
+  [data-theme="dark"]
+    --icon-color: var(--color-cream)
+    --border-active-color: var(--color-clay)
+
+  .external-link
+    &__icon svg
+      stroke: var(--icon-color)
+
+    &:hover
+      --amplitude: var(--spacing-s-000)
+      animation: excited var(--duration-running) var(--ease-peps)
+
+      :deep(.label)
+        &:after
+          height: 50%
+          left: calc(var(--border-size) * 4)
+          border-radius: calc(var(--border-size) * 4)
+          background: var(--border-active-color)
 
 </style>
