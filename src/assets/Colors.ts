30c98c43dd059214aadab5fceae2c809d0b93e83
Aurélien Grimaud <a.grimaud@involt.io>
2022-04-27 16:18:32 +0200

    Externalize the colors data

---
+++ src/assets/Colors.ts
@@ -0,0 +1,52 @@
+export const HSLColors = {
+  deepBlack: {
+    hue: 240,
+    saturation: 100,
+    lightness: 8
+  },
+  titaniumWhite: {
+    hue: 0,
+    saturation: 0,
+    lightness: 100
+  },
+  soil: {
+    hue: 340,
+    saturation: 2,
+    lightness: 30
+  },
+  sandstone: {
+    hue: 345,
+    saturation: 2,
+    lightness: 61
+  },
+  clay: {
+    hue: 3,
+    saturation: 4,
+    lightness: 78
+  },
+  cream: {
+    hue: 3,
+    saturation: 14,
+    lightness: 96
+  },
+  creamySun: {
+    hue: 49,
+    saturation: 90,
+    lightness: 62
+  },
+  softWind: {
+    hue: 209,
+    saturation: 96,
+    lightness: 89
+  },
+  candyFloss: {
+    hue: 266,
+    saturation: 60,
+    lightness: 89
+  },
+  clearWater: {
+    hue: 209,
+    saturation: 96,
+    lightness: 69
+  }
+}
