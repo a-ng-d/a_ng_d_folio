67c3434cf53ffced5736013c585b755604efe76a
Aurélien Grimaud <a.grimaud@involt.io>
2022-04-27 16:19:22 +0200

    Inject the external colors into the Landscape canvas

--- src/components/graphics/Landscape.vue
+++ src/components/graphics/Landscape.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
   import P5 from 'p5'
+  import { HSLColors } from '@/assets/Colors'
 
   export default {
     name: 'Landscape',
@@ -19,59 +20,7 @@
           mountains = [],
           clouds = []
 
-        // Style guide
-        const colors = {
-          deepBlack: {
-            hue: 240,
-            saturation: 100,
-            lightness: 8
-          },
-          titaniumWhite: {
-            hue: 0,
-            saturation: 0,
-            lightness: 100
-          },
-          soil: {
-            hue: 340,
-            saturation: 2,
-            lightness: 30
-          },
-          sandstone: {
-            hue: 345,
-            saturation: 2,
-            lightness: 61
-          },
-          clay: {
-            hue: 3,
-            saturation: 4,
-            lightness: 78
-          },
-          cream: {
-            hue: 3,
-            saturation: 14,
-            lightness: 96
-          },
-          creamySun: {
-            hue: 49,
-            saturation: 90,
-            lightness: 62
-          },
-          softWind: {
-            hue: 209,
-            saturation: 96,
-            lightness: 89
-          },
-          candyFloss: {
-            hue: 266,
-            saturation: 60,
-            lightness: 89
-          },
-          clearWater: {
-            hue: 209,
-            saturation: 96,
-            lightness: 69
-          }
-        }
+        const colors = HSLColors
 
         // Elements
         class Mountain {
