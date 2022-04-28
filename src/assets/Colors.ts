a0c7463035a183db4b7abd8abfdedde1b4f1f46b
Aurélien Grimaud <a.grimaud@involt.io>
2022-04-28 13:05:48 +0200

    Use a type for the colors

--- src/assets/Colors.ts
+++ src/assets/Colors.ts
@@ -2,51 +2,61 @@ export const HSLColors = {
   deepBlack: {
     hue: 240,
     saturation: 100,
-    lightness: 8
+    lightness: 8,
+    type: 'value'
   },
   titaniumWhite: {
     hue: 0,
     saturation: 0,
-    lightness: 100
+    lightness: 100,
+    type: 'value'
   },
   soil: {
     hue: 340,
     saturation: 2,
-    lightness: 30
+    lightness: 30,
+    type: 'greyscale'
   },
   sandstone: {
     hue: 345,
     saturation: 2,
-    lightness: 61
+    lightness: 61,
+    type: 'greyscale'
   },
   clay: {
     hue: 3,
     saturation: 4,
-    lightness: 78
+    lightness: 78,
+    type: 'greyscale'
   },
   cream: {
     hue: 3,
     saturation: 14,
-    lightness: 96
+    lightness: 96,
+    type: 'greyscale'
   },
   creamySun: {
     hue: 49,
     saturation: 90,
-    lightness: 62
+    lightness: 62,
+    type: 'primary'
   },
   softWind: {
     hue: 209,
     saturation: 96,
-    lightness: 89
+    lightness: 89,
+    type: 'primary'
   },
   candyFloss: {
     hue: 266,
     saturation: 60,
-    lightness: 89
+    lightness: 89,
+    type: 'primary'
   },
   clearWater: {
     hue: 209,
     saturation: 96,
-    lightness: 69
+    lightness: 69,
+    type: 'secondary'
   }
 }
