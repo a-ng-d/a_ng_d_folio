e5793a69bc40eb43897e029a834aae59f78a4b29
Aurélien Grimaud <a.grimaud@involt.io>
2022-08-05 21:10:20 +0200

    Make these operations global

--- src/utilities/operations.ts
+++ src/utilities/operations.ts
@@ -1,6 +1,17 @@
 export const doMap = (value, oldMin, oldMax, newMin, newMax) => {
   const oldRange = oldMax - oldMin,
         newRange = newMax - newMin
-        
+
   return ((value - oldMin) * newRange / oldRange) + newMin
+}
+
+export const random = (min, max) => Math.floor(Math.random() * (max - min)) + min
+
+export const twoRangesRandom = (rangeLeftMin, rangeLeftMax, rangeRightMin, rangeRightMax) => {
+  const randomLeft = random(rangeLeftMin, rangeLeftMax),
+        randomRight = random(rangeRightMin, rangeRightMax),
+        dice = Math.random()
+
+  const dicer = dice < .5 ? randomLeft : randomRight
+  return dicer
 }
