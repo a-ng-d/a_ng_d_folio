a3a9a5faf0cdaddecc7baf3a195b5c45de83349f
Aurélien Grimaud <a.grimaud@involt.io>
2022-08-08 02:06:07 +0200

    Build the Core page story stops

--- src/views/Core.vue
+++ src/views/Core.vue
@@ -2,7 +2,7 @@
   import Button from '@/components/ui/Button.vue'
   import ScrollingText from '@/components/ui/ScrollingText.vue'
   import Footer from '@/components/patterns/Footer.vue'
-  import { doMap } from '@/utilities/operations'
+  import { doMap, random, twoRangesRandom } from '@/utilities/operations'
 
   export default {
     name: 'Core',
@@ -23,7 +23,8 @@
       return {
         triggers: [],
         section: 'section-1',
-        scale: 1,
+        perspective: 300,
+        translation: 0,
         opacity: 1
       }
     },
@@ -31,11 +32,11 @@
       scrollProgress(to, from) {
         this.triggers.forEach((trigger, index) => {
           if (to >= trigger.top && to < trigger.bottom) {
-            const startScaling = index == 0 ? 1 : index == this.triggers.length - 1 ? 0 : 0,
-                  endScaling = index == 0 ? 5 : index == this.triggers.length - 1 ? 10 : 5
+            const startScaling = index == 0 ? 0 : -this.perspective,
+                  endScaling = this.perspective
             this.section = `section-${index + 1}`
-            this.scale = doMap(to, trigger.top, trigger.bottom, startScaling, endScaling)
-            to > trigger.middle ? this.opacity = doMap(to, trigger.middle, trigger.bottom, 1, 0) : this.opacity = 1
+            this.translation = doMap(to, trigger.top, trigger.bottom, startScaling, endScaling)
+            to < trigger.middle ? index != 0 ? this.opacity = doMap(to, trigger.top, trigger.middle, 0, 1) : this.opacity = 1 : this.opacity = 1
           }
         })
       }
@@ -59,89 +60,145 @@
   <main class="page">
     <article class="story">
       <section class="story__intro">
-        <div class="story__content--jumbotron" :class="section === 'section-1' ? 'story__content--moving' : null">
-          <Transition name="slide-up" style="--delay: var(--delay-turtoise)" appear>
-            <ScrollingText
-              :label="$t('core.intro.title')"
-              :theme="theme"
-            />
-          </Transition>
-          <Transition name="slide-up" style="--delay: calc(var(--delay-turtoise) + (var(--duration-step) * .5))" appear>
-            <ScrollingText
-              :label="$t('core.intro.subtitle')"
-              direction="RIGHT"
-              isSubTitle
-              :theme="theme"
-              style="margin-left: calc(var(--sizing-s-000) * -1)"
-            />
-          </Transition>
+        <div class="story__content" :class="section === 'section-1' ? 'story__content--centered' : null">
+          <div class="story__content--only-ground">
+            <Transition name="slide-up" style="--delay: var(--delay-turtoise)" appear>
+              <ScrollingText
+                :label="$t('core.intro.title')"
+                :theme="theme"
+              />
+            </Transition>
+            <Transition name="slide-up" style="--delay: calc(var(--delay-turtoise) + (var(--duration-step) * .5))" appear>
+              <ScrollingText
+                :label="$t('core.intro.subtitle')"
+                direction="RIGHT"
+                isSubTitle
+                :theme="theme"
+                style="margin-left: calc(var(--sizing-s-000) * -1)"
+              />
+            </Transition>
+          </div>
         </div>
       </section>
       <section class="story__stop">
-        <div class="story__content" :class="section === 'section-2' ? 'story__content--moving' : null">
-          <ScrollingText
-            :label="$t('core.productDesign.title')"
-            :theme="theme"
-          />
+        <div class="story__content" :class="section === 'section-2' ? 'story__content--centered' : null">
+          <div class="story__content--first-ground">
+            <h1>{{ $t('core.productDesign.title') }}</h1>
+          </div>
+          <div class="story__content--second-ground">
+            <h3>{{ $t('core.productDesign.summary') }}</h3>
+          </div>
+          <div class="story__content--third-ground">
+            <h6>{{ $t('core.productDesign.mission-1') }}</h6>
+          </div>
+          <div class="story__content--fourth-ground">
+            <h6>{{ $t('core.productDesign.mission-2') }}</h6>
+          </div>
+          <div class="story__content--fiveth-ground">
+            <h6>{{ $t('core.productDesign.mission-3') }}</h6>
+          </div>
         </div>
       </section>
       <section class="story__stop">
-        <div class="story__content" :class="section === 'section-3' ? 'story__content--moving' : null">
-          <ScrollingText
-            :label="$t('core.creativeTechnology.title')"
-            :theme="theme"
-          />
+        <div class="story__content" :class="section === 'section-3' ? 'story__content--centered' : null">
+          <div class="story__content--first-ground">
+            <h1>{{ $t('core.creativeTechnology.title') }}</h1>
+          </div>
+          <div class="story__content--second-ground">
+            <h3>{{ $t('core.creativeTechnology.summary') }}</h3>
+          </div>
+          <div class="story__content--third-ground">
+            <h6>{{ $t('core.creativeTechnology.mission-1') }}</h6>
+          </div>
+          <div class="story__content--fourth-ground">
+            <h6>{{ $t('core.creativeTechnology.mission-2') }}</h6>
+          </div>
+          <div class="story__content--fiveth-ground">
+            <h6>{{ $t('core.creativeTechnology.mission-3') }}</h6>
+          </div>
         </div>
       </section>
       <section class="story__stop">
-        <div class="story__content" :class="section === 'section-4' ? 'story__content--moving' : null">
-          <ScrollingText
-            :label="$t('core.facilitation.title')"
-            :theme="theme"
-          />
+        <div class="story__content" :class="section === 'section-4' ? 'story__content--centered' : null">
+          <div class="story__content--first-ground">
+            <h1>{{ $t('core.facilitation.title') }}</h1>
+          </div>
+          <div class="story__content--second-ground">
+            <h3>{{ $t('core.facilitation.summary') }}</h3>
+          </div>
+          <div class="story__content--third-ground">
+            <h6>{{ $t('core.facilitation.mission-1') }}</h6>
+          </div>
+          <div class="story__content--fourth-ground">
+            <h6>{{ $t('core.facilitation.mission-2') }}</h6>
+          </div>
+          <div class="story__content--fiveth-ground">
+            <h6>{{ $t('core.facilitation.mission-3') }}</h6>
+          </div>
         </div>
       </section>
       <section class="story__stop">
-        <div class="story__content" :class="section === 'section-5' ? 'story__content--moving' : null">
-          <ScrollingText
-            :label="$t('core.communication.title')"
-            :theme="theme"
-          />
+        <div class="story__content" :class="section === 'section-5' ? 'story__content--centered' : null">
+          <div class="story__content--first-ground">
+            <h1>{{ $t('core.communication.title') }}</h1>
+          </div>
+          <div class="story__content--second-ground">
+            <h3>{{ $t('core.communication.summary') }}</h3>
+          </div>
+          <div class="story__content--third-ground">
+            <h6>{{ $t('core.communication.mission-1') }}</h6>
+          </div>
+          <div class="story__content--fourth-ground">
+            <h6>{{ $t('core.communication.mission-2') }}</h6>
+          </div>
+          <div class="story__content--fiveth-ground">
+            <h6>{{ $t('core.communication.mission-3') }}</h6>
+          </div>
         </div>
       </section>
       <section class="story__stop">
-        <div class="story__content" :class="section === 'section-6' ? 'story__content--moving' : null">
-          <ScrollingText
-            :label="$t('core.participatory.title')"
-            :theme="theme"
-          />
+        <div class="story__content" :class="section === 'section-6' ? 'story__content--centered' : null">
+          <div class="story__content--first-ground">
+            <h1>{{ $t('core.human.title') }}</h1>
+          </div>
+          <div class="story__content--second-ground">
+            <h3>{{ $t('core.human.summary') }}</h3>
+          </div>
+          <div class="story__content--third-ground">
+            <h6>{{ $t('core.human.mission-1') }}</h6>
+          </div>
+          <div class="story__content--fourth-ground">
+            <h6>{{ $t('core.human.mission-2') }}</h6>
+          </div>
+          <div class="story__content--fiveth-ground">
+            <h6>{{ $t('core.human.mission-3') }}</h6>
+          </div>
         </div>
       </section>
       <section class="story__stop">
-        <div class="story__content" :class="section === 'section-7' ? 'story__content--moving' : null">
-          <ScrollingText
-            :label="$t('core.sustainable.title')"
-            :theme="theme"
-          />
+        <div class="story__content" :class="section === 'section-7' ? 'story__content--centered' : null">
+          <div class="story__content--first-ground">
+            <h1>{{ $t('core.extra.title') }}</h1>
+          </div>
+          <div class="story__content--second-ground">
+            <h3>{{ $t('core.extra.summary') }}</h3>
+          </div>
+          <div class="story__content--third-ground">
+            <h6>{{ $t('core.extra.mission-1') }}</h6>
+          </div>
+          <div class="story__content--fourth-ground">
+            <h6>{{ $t('core.extra.mission-2') }}</h6>
+          </div>
+          <div class="story__content--fiveth-ground">
+            <h6>{{ $t('core.extra.mission-3') }}</h6>
+          </div>
         </div>
       </section>
-      <section class="story__outro">
-        <div class="story__content--jumbotron" :class="section === 'section-8' ? 'story__content--moving' : null">
-          <Transition name="slide-up" style="--delay: var(--delay-turtoise)" appear>
-            <ScrollingText
-              :label="'bye_bye_'"
-              :theme="theme"
-            />
-          </Transition>
-          <Transition name="slide-up" style="--delay: calc(var(--delay-turtoise) + (var(--duration-step) * .5))" appear>
-            <ScrollingText
-              :label="'byebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebyebye'"
-              direction="RIGHT"
-              isSubTitle
-              :theme="theme"
-              style="margin-left: calc(var(--sizing-s-000) * -1)"
-            />
-          </Transition>
+      <section class="story__stop">
+        <div class="story__content" :class="section === 'section-8' ? 'story__content--centered' : null">
+          <div class="story__content--first-ground">
+            <h1>Bye</h1>
+          </div>
         </div>
       </section>
     </article>
@@ -163,26 +220,58 @@
     &[data-theme="DARK"]
       --text-color: var(--color-cream)
 
-    &__intro, &__outro
+    &__intro
       height: 100vh
 
     &__stop
-      height: 200vh
+      height: 300vh
 
     &__content
-      transform: scale(0)
+      opacity: v-bind("section != 'section-1' ? 0 : 1")
+      visibility: hidden
 
-      &--jumbotron
-        display: flex
-        flex-flow: column nowrap
-        gap: var(--layout-row-gap) 0
-        transform: scale(0)
-
-      &--moving
+      &--centered
         position: fixed
         top: 50%
         left: 50%
-        transform: translate(-50%, -50%) scale(v-bind("scale"))
-        opacity: v-bind("opacity")
+        transform: translate(-50%, -50%)
         pointer-events: none
+        opacity: 1
+        display: flex
+        justify-content: center
+        align-items: center
+        flex-flow: column
+        visibility: visible
+
+      &--only-ground
+        transform: perspective(v-bind("perspective + 'vh'")) translate3d(0, 0, v-bind("translation + 'vh'"))
+        opacity: 1
+        display: flex
+        flex-flow: column nowrap
+        gap: var(--layout-row-gap) 0
+
+      &--first-ground
+        transform: perspective(v-bind("perspective * .1 + 'vh'")) translate3d(0, 0, v-bind("translation + 'vh'"))
+        opacity: v-bind("opacity")
+        order: 2
+
+      &--second-ground
+        transform: perspective(v-bind("perspective *.2 + 'vh'")) translate3d(0, 0, v-bind("translation + 'vh'"))
+        opacity: v-bind("opacity")
+        order: 3
+
+      &--third-ground
+        transform: perspective(v-bind("perspective * .8 + 'vh'")) translate3d(0, 0, v-bind("translation + 'vh'"))
+        opacity: v-bind("opacity")
+        order: 1
+
+      &--fourth-ground
+        transform: perspective(v-bind("perspective * .9 + 'vh'")) translate3d(0, 100%, v-bind("translation + 'vh'"))
+        opacity: v-bind("opacity")
+        order: 1
+
+      &--fiveth-ground
+        transform: perspective(v-bind("perspective + 'vh'")) translate3d(0, 200%, v-bind("translation + 'vh'"))
+        opacity: v-bind("opacity")
+        order: 1
 </style>
