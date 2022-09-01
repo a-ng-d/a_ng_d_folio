2fbdbf2bdcbe2fa5cab27cd65dbab3903e6bb190
Aurélien Grimaud <a.grimaud@involt.io>
2022-09-01 04:00:07 +0200

    Set a loading page before mounting app

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import router from './router'
 import { i18n } from './lang'
 import Vue3Lottie from 'vue3-lottie'
 
-createApp(App)
-  .use(router)
-  .use(i18n)
-  .use(Vue3Lottie)
-  .mount('#app')
+const app = createApp(App)
+
+window.onload = () => {
+  setTimeout(() => document.getElementById('loader').classList.replace('loader--enter', 'loader--leave'), 3000)
+  setTimeout(() => document.getElementById('loader').innerHTML = '', 3400)
+  setTimeout(() => app
+    .use(router)
+    .use(i18n)
+    .use(Vue3Lottie)
+    .mount('#app')
+  , 3400)
+}
