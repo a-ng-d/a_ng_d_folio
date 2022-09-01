6623f1d94c39d9aad3c567431fa833539fcdef4f
Aurélien Grimaud <a.grimaud@involt.io>
2022-09-01 16:29:41 +0200

    Declare the variable at a higher level

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import router from './router'
 import { i18n } from './lang'
 import Vue3Lottie from 'vue3-lottie'
 
-const app = createApp(App)
+const app = createApp(App),
+loader: HTMLElement = document.getElementById('loader')!
 
 window.onload = () => {
-  const loader: HTMLElement = document.getElementById('loader')!
   setTimeout(() => loader.classList.replace('loader--enter', 'loader--leave'), 3000)
   setTimeout(() => loader.innerHTML = '', 3400)
   setTimeout(() => app
