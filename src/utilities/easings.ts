85edfcb28f4c996d99d099d593a5a88af6813419
Aurélien Grimaud <a.grimaud@involt.io>
2022-09-03 01:14:23 +0200

    Get newer easings functions

--- src/utilities/easings.ts
+++ src/utilities/easings.ts
@@ -5,7 +5,6 @@ export const easeInOutExpo = (t: number, b: number, c: number, d: number) => {
   return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b
 }
 
-export const easeInOutCubic = (t: number, b: number, c: number, d: number) => {
-    if ((t /= d / 2) < 1) return c / 2 * t * t * t + b
-    return c / 2 * ((t -= 2) * t * t + 2) + b
-}
+export const easeInOutCubic: number = (x: number) => x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2
+
+export const easeInOutQuart: number = (x: number) => x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2
