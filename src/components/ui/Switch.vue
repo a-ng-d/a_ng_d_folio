83d37a46d80451abe15246ef66806a7ff3de73cb
Aurélien Grimaud <a.grimaud@involt.io>
2022-09-06 02:52:53 +0200

    Add a new Switch component

---
+++ src/components/ui/Switch.vue
@@ -0,0 +1,140 @@
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import Particles from '@/components/graphics/Particles.vue'
+  import { doMap } from '@/utilities/operations'
+
+  export default defineComponent({
+    name: 'Switch',
+    components: {
+      Particles
+    },
+    props: {
+      label: String,
+      state: {
+        type: Boolean,
+        default: false
+      },
+      on: {
+        type: Function,
+        required: true
+      },
+      off: {
+        type: Function,
+        required: true
+      },
+      theme: {
+        type: String,
+        default: 'DEFAULT'
+      }
+    },
+    data: function() {
+      return {
+        isActive: this.state
+      }
+    },
+    methods: {
+      moveSwitch() {
+        this.isActive = !this.isActive
+        this.isActive ? this.on() : this.off()
+      }
+    }
+  })
+</script>
+
+<template>
+  <div class="switch">
+    <label v-if="label != undefined" class="switch__label" for="swt"><p>{{ label }}</p></label>
+    <input type="checkbox" name="swt" @click="moveSwitch" />
+    <div class="switch__indicator">
+      <div class="switch__slide">
+        <div class="switch__knob"></div>
+        <div class="switch__background">
+          <Particles
+            :weight="16"
+            :isExpanded="isActive"
+            borderRadius="calc(var(--button-height-size) / 4)"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="sass">
+  // Structure
+  .switch
+    display: flex
+    gap: 0 var(--layout-column-gap)
+
+    &__slide
+      --slide-padding: var(--spacing-xs-000)
+
+      width: var(--sizing-xl-200)
+      height: calc(var(--button-height-size) / 2)
+      border-radius: calc(var(--button-height-size) / 4)
+      background-color: var(--color-cream)
+      padding: var(--slide-padding)
+      box-shadow: 0 0 0 var(--sizing-xs-000) var(--slide-border)
+
+    &__knob
+      width: var(--spacing-l-000)
+      height: calc((var(--button-height-size) / 2) - (var(--slide-padding) * 2))
+      border-radius: calc(((var(--button-height-size) / 2) - (var(--slide-padding) * 2)) / 2)
+      background-color: var(--knob-background)
+      z-index: 1
+      animation: smooth-slide-off var(--duration-running) var(--ease-vroom) forwards
+
+    &__background
+      position: absolute
+      width: 100%
+      height: 100%
+      top: 0
+      left: 0
+
+    input
+      position: absolute
+      width: 100%
+      height: 100%
+      z-index: 2
+      appearance: unset
+      top: 0
+      left: 0
+      cursor: pointer
+
+  // Aspect
+  .switch
+    &__indicator
+      --slide-background: transparent
+      --slide-border: var(--color-soil)
+      --knob-background: var(--color-clay)
+
+  // Event
+  .switch
+    input:checked ~ &__indicator
+      --knob-background: var(--color-soil)
+
+    input:checked ~ &__indicator &__knob
+      animation: smooth-slide-on var(--duration-running) var(--ease-vroom) forwards
+
+  @keyframes smooth-slide-on
+    0%
+      transform: translateX(0) scale(1, 1)
+      animation-timing-function: ease-in
+    50%
+      transform: translateX(20rem) scale(1.4, .6)
+      animation-timing-function: linear
+    100%
+      transform: translateX(40rem) scale(1, 1)
+      animation-timing-function: ease-out
+
+  @keyframes smooth-slide-off
+    0%
+      transform: translateX(40rem) scale(1, 1)
+      animation-timing-function: ease-in
+    50%
+      transform: translateX(20rem) scale(1.4, .6)
+      animation-timing-function: linear
+    100%
+      transform: translateX(0rem) scale(1, 1)
+      animation-timing-function: ease-out
+</style>
