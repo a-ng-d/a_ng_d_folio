e4045356fb968a90bd0aadfed78ad5dbb35a5593
Aurélien Grimaud <a.grimaud@involt.io>
2022-09-22 23:05:39 +0200

    Fix a starting bug

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -70,14 +70,6 @@
         >
           <template #plain>
             <i18n-t :keypath="`work.${project.codeName}.description`" tag="p" class="enhanced">
-              <template v-slot:link-1>
-                <SimpleExternalLink
-                  :label="$t(`work.${project.codeName}.externalLinks.link-1`)"
-                  href="/"
-                  alt=""
-                  :theme="theme"
-                />
-              </template>
             </i18n-t>
           </template>
         </OneColumn>
