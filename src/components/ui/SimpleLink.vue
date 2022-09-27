095beb59d5c7ee1a07461865021a718d3768e5b6
Aurélien Grimaud <a.grimaud@involt.io>
2022-09-27 03:32:41 +0200

    Create a SimpleLink

---
+++ src/components/ui/SimpleLink.vue
@@ -0,0 +1,105 @@
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import { store } from '@/utilities/store'
+  import Label from '@/components/ui/Label.vue'
+
+  export default defineComponent({
+    name: 'SimpleLink',
+    components: {
+      Label
+    },
+    props: {
+      label: {
+        type: String,
+        required: true
+      },
+      path: String,
+      alt: String,
+      small: {
+        type: Boolean,
+        default: false
+      },
+      large: {
+        type: Boolean,
+        default: false
+      },
+      theme: {
+        type: String,
+        default: 'DEFAULT'
+      }
+    },
+    data: function() {
+      return {
+        store
+      }
+    }
+  })
+</script>
+
+<template>
+  <RouterLink
+    class="link"
+    :to="path"
+    :alt="alt"
+    @focus="store.isFocus = true"
+    @blur="store.isFocus = false"
+    :data-theme="theme"
+  >
+    <div class="link__label">
+      <Label
+        :label="label"
+        underlined
+        :small="small ? true : false"
+        :large="large ? true : false"
+        :theme="theme"
+      />
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped lang="sass">
+  // Structure
+  .link
+    display: inline-flex
+    gap: v-bind("small ? 'calc(var(--rich-external-link-gap) * .75)' : large ? 'calc(var(--rich-external-link-gap) * 1.5)' : 'var(--rich-external-link-gap)'")
+    transition: var(--simple-transition)
+    align-items: baseline
+    transform-origin: center center
+
+  // Aspect
+  .link
+    &[data-theme="DARK"]
+      --icon-color: var(--color-cream)
+      --border-active-color: var(--color-clay)
+
+  // Events
+  .link
+    &:hover
+      --amplitude: var(--spacing-s-000)
+
+      animation: excited var(--duration-running) var(--ease-peps)
+
+      .label
+        &:after
+          --border-color: var(--border-active-color)
+
+          height: 75%
+          left: calc(var(--border-size) * 4)
+
+    &:focus
+      transform: var(--focus-scale)
+      padding: 0 var(--spacing-m-000)
+
+    &:active
+      transform: var(--active-scale)
+
+    &:focus, &:active
+      z-index: 2
+
+      .label
+        &:after
+          --border-color: var(--border-focus-color)
+
+          height: 75%
+          left: calc(var(--border-size) * 4)
+</style>
