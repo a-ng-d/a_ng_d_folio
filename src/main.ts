50c33288b094b39d963e07f9b5ac0a4e383377c2
Aurélien Grimaud <a.grimaud@involt.io>
2023-09-04 15:40:40 +0200

    Skip loader in dev environment

--- src/main.ts
+++ src/main.ts
@@ -13,54 +13,60 @@ const app = createApp(App),
 
 document.title = 'Virtualization in progress…'
 
-// Progress bar
-NProgress.configure({
-  showSpinner: false,
-  parent: '#progress',
-  easing: 'ease',
-  speed: 200,
-})
-NProgress.start()
-
 // Cursor
 SetCursor()
 
-// Loading screen
-window.onload = () => {
-  let isFrozen = false
+if (import.meta.env.MODE != 'development') {
+  // Progress bar
+  NProgress.configure({
+    showSpinner: false,
+    parent: '#progress',
+    easing: 'ease',
+    speed: 200,
+  })
+  NProgress.start()
 
-  Loop.playSegments([[0, 200]], false)
+  // Loading screen
+  window.onload = () => {
+    let isFrozen = false
 
-  Loop.onLoopComplete = () => {
-    isFrozen = !isFrozen
-    if (isFrozen) {
-      Loop.goToAndStop(200, true)
-      loader.classList.add('loader--loaded')
-      document.onkeyup = () => entrance()
-      document.body.ontouchstart = () => entrance()
-      NProgress.done()
-    } else Loop.goToAndStop(420, true)
+    Loop.playSegments([[0, 200]], false)
+
+    Loop.onLoopComplete = () => {
+      isFrozen = !isFrozen
+      if (isFrozen) {
+        Loop.goToAndStop(200, true)
+        loader.classList.add('loader--loaded')
+        document.onkeyup = () => entrance()
+        document.body.ontouchstart = () => entrance()
+        NProgress.done()
+      } else Loop.goToAndStop(420, true)
+    }
   }
-}
 
-const entrance = (): void => {
-  Loop.setSpeed(2)
-  Loop.playSegments([[200, 420]], false)
-  Loop.play()
-  feedback.volume = 0.2
-  document.body.clientWidth > 1280 ? feedback.play() : null
-  document.onkeyup = null
-  document.body.ontouchstart = null
+  const entrance = (): void => {
+    Loop.setSpeed(2)
+    Loop.playSegments([[200, 420]], false)
+    Loop.play()
+    feedback.volume = 0.2
+    document.body.clientWidth > 1280 ? feedback.play() : null
+    document.onkeyup = null
+    document.body.ontouchstart = null
 
-  loader.classList.remove('loader--loaded')
+    loader.classList.remove('loader--loaded')
 
-  setTimeout(
-    () => loader.classList.replace('loader--enter', 'loader--leave'),
-    2000
-  )
-  setTimeout(() => {
-    Loop.destroy()
-    loader.innerHTML = ''
-    app.use(router).use(i18n).use(Vue3Lottie).mount('#app')
-  }, 3600)
+    setTimeout(
+      () => loader.classList.replace('loader--enter', 'loader--leave'),
+      2000
+    )
+    setTimeout(() => {
+      Loop.destroy()
+      loader.remove()
+      app.use(router).use(i18n).use(Vue3Lottie).mount('#app')
+    }, 3600)
+  }
+} else {
+  Loop.destroy()
+  loader.remove()
+  app.use(router).use(i18n).use(Vue3Lottie).mount('#app')
 }
